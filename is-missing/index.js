c7b9ef2ebdcb61b9c4743ff7e76c23515f777f8f
tunnckoCore <mameto_100@mail.bg>
2015-05-11 06:23:35 +0300

    is-missing example

---
+++ is-missing/index.js
@@ -0,0 +1,12 @@
+/*!
+ * is-missing <https://github.com/tunnckoCore/is-missing>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function isMissing () {
+  // body
+}
---
+++ is-missing/test.js
@@ -0,0 +1,15 @@
+/*!
+ * is-missing <https://github.com/tunnckoCore/is-missing>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+var test = require('assertit')
+var isMissing = require('./index')
+
+test('is-missing:', function () {
+  // body
+})
